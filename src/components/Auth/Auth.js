7958661a3d2404bc8ed4e34303ee37a3adacf257
Digory Ellsworth <ellsworth.digory@gmail.com>
2018-11-08 14:47:27 -0700

    .

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import Blur from 'react-blur';
+// import Blur from 'react-blur';
 import './Auth.css';
 
 class Auth extends Component {
@@ -27,20 +27,18 @@ class Auth extends Component {
     login() {
         let { REACT_APP_DOMAIN, REACT_APP_CLIENT_ID } = process.env;
         let url = `${encodeURIComponent(window.location.origin)}/auth/callback`;
-        window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`
+        window.location = `http://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`
     }
 
     render() {
         return(
             <section>
-                <Blur className="Body" id='body' img={this.state.img} blurRadius={200}>
                     <div className="Header-Auth" id="Auth">
                         <h1 className='auth-title'>idigyo / Words</h1>
                         <div className="button-box">
                             <button id="darkbuttons" onClick={this.login}> Login </button>
                         </div>
                     </div>
-                </Blur>
             </section>
         )
     }
